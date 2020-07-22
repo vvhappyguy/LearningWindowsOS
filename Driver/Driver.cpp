28782091b55802b8ba6ebf3019be62113d6631a0
Ivan Dyakin <vvhappyguy@gmail.com>
2020-07-22 11:24:57 +0300

    Init project with gitignore.

---
+++ Driver/Driver.cpp
@@ -0,0 +1,10 @@
+#include <ntddk.h>
+
+extern "C"
+NTSTATUS DriverEntry(_In_ PDRIVER_OBJECT DriverObject, _In_ PUNICODE_STRING RegistryPath)
+{
+	UNREFERENCED_PARAMETER(DriverObject);
+	UNREFERENCED_PARAMETER(RegistryPath);
+	
+	return STATUS_SUCCESS;
+};
