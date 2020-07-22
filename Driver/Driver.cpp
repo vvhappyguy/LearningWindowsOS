68ce8b509bcdec334249077f4440aad578ab740f
Ivan Dyakin <vvhappyguy@gmail.com>
2020-07-22 11:26:16 +0300

    Added unload routine.

--- Driver/Driver.cpp
+++ Driver/Driver.cpp
@@ -1,10 +1,20 @@
 #include <ntddk.h>
 
+void SampleUnload(_In_ PDRIVER_OBJECT DriverObject);
+
 extern "C"
 NTSTATUS DriverEntry(_In_ PDRIVER_OBJECT DriverObject, _In_ PUNICODE_STRING RegistryPath)
 {
 	UNREFERENCED_PARAMETER(DriverObject);
 	UNREFERENCED_PARAMETER(RegistryPath);
+
+	DriverObject->DriverUnload = SampleUnload;
 	
 	return STATUS_SUCCESS;
 };
+
+
+void SampleUnload(_In_ PDRIVER_OBJECT DriverObject) 
+{
+	UNREFERENCED_PARAMETER(DriverObject);
+}
